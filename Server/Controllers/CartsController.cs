fc718377bb6a535b7f4c2b1dd40f40eed0669638
haiwen23232 <zhouhaiw@deakin.edu.au>
2019-09-18 16:36:31 +1000

    cart

---
+++ Server/Controllers/CartsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class CartsController : DbConnection
+    {
+        [HttpGet]
+        public List<Cart> GetAll()
+        {
+            List<Cart> carts = Context.Carts.ToList();
+            for (int i = 0; i < carts.Count; i++)
+            {
+                carts[i].User = Context.Users.SingleOrDefault(u => u.UserId == carts[i].UserId);
+                carts[i].Product = Context.Products.SingleOrDefault(p => p.ProductId == carts[i].ProductId);
+                carts[i].Product.Category =
+                    Context.Categories.SingleOrDefault(c => c.CategoryId == carts[i].Product.CategoryId);
+            }
+            return carts;
+        }
+
+        [HttpGet("GetByUser/{userID}")]
+        public List<Cart> GetByUser(string userId)
+        {
+            List<Cart> carts = Context.Carts.Where(c => c.UserId == Int32.Parse(userId)).ToList();
+            for (int i = 0; i < carts.Count; i++)
+            {
+                carts[i].User = Context.Users.SingleOrDefault(u => u.UserId == carts[i].UserId);
+                carts[i].Product = Context.Products.SingleOrDefault(p => p.ProductId == carts[i].ProductId);
+                carts[i].Product.Category =
+                    Context.Categories.SingleOrDefault(c => c.CategoryId == carts[i].Product.CategoryId);
+            }
+            return carts;
+        }
+
+        [HttpPost]
+        public Cart CreateCart([FromBody] Cart cart)
+        {
+            Context.Carts.Add(cart);
+            Context.SaveChanges();
+            return cart;
+        }
+
+    }
+}
---
+++ Server/Models/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int CartId { get; set; }
+        public int Quantity { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
